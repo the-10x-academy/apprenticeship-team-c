9f8db6943c39ce51fed55e86d4472f537665db44
Deepak <deepakyadav301199@gmail.om>
2021-01-29 14:24:26 +0530

    header part and css

---
+++ client/src/Components/HomePage/header.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import "./header.css";
+import clone from "./images/icon.png";
+import camera from "./images/camera.png"
+
+class Navbar extends Component{
+    render(){
+        return(
+            <>
+                <nav className="NavbarItems">
+                    <img className="Navbar-logo" src={clone} alt="logo" ></img>
+                    <p className="instaclone">Instaclone</p>
+                    <img className="camera" src={camera} alt="camera" ></img>
+                </nav>
+            </>
+        )
+    }
+}
+
+export default Navbar;
