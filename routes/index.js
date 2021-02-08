10ba95be8f79ef845bcc40a01c5fd9958cb7bacd
Deepak <deepakyadav301199@gmail.om>
2021-02-08 11:03:46 +0530

    some change in post upload page and new schema for upload details

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,23 @@
 const express = require("express");
 const router = express.Router();
 const Post = require("../models/post");
+const uploadModel = require("./uploads");
+
+/* data Save */
+router.post("./upload", upload, function(req, res, next){
+    const imageFile = req.file.filename+"uploaded successfully";
+
+    const imageDetails = new uploadModel({
+        imageName : imageFile
+    });
+    imageDetails.save(function(err, docs){
+        if(err) throw err;
+        
+        res.render("upload-file", {title: "files", success:success})
+    })
+})
+
+
 /* get post */
 function  getPost(res) {
     Post.find({}, function(err, docs){
