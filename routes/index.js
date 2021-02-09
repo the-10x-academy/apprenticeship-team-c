8c296b0072ed2ab71b338bd3ff10f7f277e697dd
Prabal <sprabal01@gmail.com>
2021-02-09 12:08:26 +0530

    implemented multer

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,18 @@ const router = express.Router();
 const Post = require("../models/post");
 var cors = require('cors')
 var app = express();
-
+var multer  = require('multer')
+var upload = multer({ dest: './public/images' })
 // app.use(express.static("public"))
 
 app.use(cors())
 
-router.post("/upload", function(req,res,next) {
-    res.send("hey")
+router.post("/upload",  upload.single('file'),function(req,res,next) {
+    console.log("author :",req.body.author)
+    console.log("location ",req.body.location)
+    console.log("file:" ,req.file)
+    
+    
 })
 
 
