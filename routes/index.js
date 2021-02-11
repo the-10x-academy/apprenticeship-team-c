757bb3171a5ed1f918edd51870c6ef5a0e85d4f9
Deepak <deepakyadav301199@gmail.om>
2021-02-11 20:45:36 +0530

    resolved cors error

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const Post = require("../models/post");
-var app = express();
+
 var multer  = require('multer')
 var upload = multer({ dest: './public/images' })
-var cors = require('cors')
 
-app.use(cors())
 
 router.post("/upload",  upload.single('file'),function(req,res,next) {
     var data = {
